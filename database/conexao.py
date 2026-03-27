10696c4ca158fad3baaa9aed57ee0f9d165e7c53
Luiza Rangel <luiza.g.rangel@aluno.senai.br>
2026-03-27 16:00:02 -0300

    commity

--- database/conexao.py
+++ database/conexao.py
@@ -1,13 +1,25 @@
 import mysql.connector
 
+conexao_banco = "LOCAL"
+
 def conectar():
-        conexao = mysql.connector.connect(
-                host="localhost",
-                port=3306,
-                user="root",
-                password= "root",
-                database= "carrapichoTube"
-            )
+        if conexao_banco == "LOCAL":
+                
+            conexao = mysql.connector.connect(
+                    host="localhost",
+                    port=3306,
+                    user="root",
+                    password= "root",
+                    database= "carrapichoTube"
+                )
+        else:
+              conexao = mysql.connector.connect(
+                    host="servidor-luizarangel-luizarangel.a.aivencloud.com",
+                    port=20659,
+                    user="avnadmin",
+                    password= "<redacted>",
+                    database= "defaultdb"
+                    )
             #criando cursor
         cursor = conexao.cursor(dictionary=True)
 
